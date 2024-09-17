68c00274b740156f82fa6b6a7851fd0bd9086a6a
leetaam.lt <leetaam.lt@gmail.com>
2024-09-17 15:40:58 +0700

    update

--- app/src/Controller/OrderController.php
+++ app/src/Controller/OrderController.php
@@ -275,6 +275,12 @@ class OrderController
 
     private function send_news_offers($email)
     {
+        if (!class_exists('WPCF7_ContactForm')) {
+            error_log('Sending email error: 500');
+        }
+        if (!class_exists('WPCF7_Submission')) {
+            error_log('Sending email error: 500');
+        }
         $contact_form = \WPCF7_ContactForm::get_instance(11); // ID của mẫu Contact Form 7
         $submission = \WPCF7_Submission::get_instance();
         if ($submission) {
