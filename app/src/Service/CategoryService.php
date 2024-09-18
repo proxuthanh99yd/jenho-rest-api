669205d96b434fd2eba6f9fa8b0aebf081dee7c5
leetaam.lt <leetaam.lt@gmail.com>
2024-09-18 16:29:44 +0700

    update

--- app/src/Service/CategoryService.php
+++ app/src/Service/CategoryService.php
@@ -18,7 +18,7 @@ class CategoryService
         foreach ($termFields as $term_id) {
             $cat_thumb_id = get_woocommerce_term_meta($term_id, 'thumbnail_id', true);
             $cat_thumb_url = wp_get_attachment_thumb_url($cat_thumb_id);
-            $theTerm =  get_term_by('term_id', $term_id, $this->tax);
+            $theTerm =  get_term_by('term_id', $term_id, $this->tax, ARRAY_A);
             $theTerm['thumbnail'] = $cat_thumb_url;
             $response[] = $theTerm;
         }
